d1b677d1770625f77724d258efa0b5b5832f8a5e
Alfer66 <95257944+Alfer66@users.noreply.github.com>
2023-02-13 18:46:57 -0600

    Add files via upload

---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html',
+  styles: []
+})
+export class ProtegidaComponent {
+
+}
